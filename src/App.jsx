48a6afd8cbe18c4ace0d47eca573f9c62c6d8d04
avishkar durgade <avishkardurgade@gmail.com>
2025-03-04 14:29:36 +0530

    CAcomplete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ServiceCard  from './components/ServiceCard'
+
+
+function App() {
+  return (
+    <>
+    <ServiceCard title="Web Development" description="we are providing complete web development course which covers in depth concepts"/>
+    <br /><br /><br /><br />
+    <ServiceCard title="Complete DSA" description="we are providing an in depth course on complete mastery in DSA "/>
+    </>
+  )
+}
+
+export default App
